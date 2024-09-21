9a4b524f5dd1d8e48db9d4aecffbb959ba028b20
Alessio Boscolo <73670663+AlessioBoscolo@users.noreply.github.com>
2024-09-21 21:34:19 +0200

    Created the main interface
    interface with two dice and one button

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageView1.image = UIImage(named: "DiceTwo")
+        diceImageView2.image = UIImage(named: "DiceThree")
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Button got tapped.")
+    }
 }
 
